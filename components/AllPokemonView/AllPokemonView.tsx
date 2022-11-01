4f3837e1d076948bb228575d74119c42a77487a1
javonmcgilberry <javonmcgilberry@icloud.com>
2022-11-01 11:22:33 -0400

    fix small key issue

--- components/AllPokemonView/AllPokemonView.tsx
+++ components/AllPokemonView/AllPokemonView.tsx
@@ -1,5 +1,6 @@
 import { GetAllPokemonSpeciesQuery } from '@pokedex/generated/graphql-hooks'
 import { HandleSetActivePokemon } from '@pokedex/types/types'
+import { Fragment } from 'react'
 import { Pagination } from '../Pagination/Pagination'
 import PokemonCard from '../PokemonCard/PokemonCard'
 
@@ -24,8 +25,8 @@ const AllPokemonView = ({
     <>
       <div className="relative grid grid-cols-3 gap-4 drop-shadow-md">
         {isAllPokemonDataLoading &&
-          [...Array(3)].map((el) => (
-            <>
+          [...Array(3)].map((el, index) => (
+            <Fragment key={index}>
               <div
                 style={{
                   paddingTop: '100%',
@@ -34,7 +35,7 @@ const AllPokemonView = ({
                 className={`relative flex h-0 w-full animate-pulse flex-col items-center rounded-[4rem] bg-opacity-50 p-4`}
                 key={el}
               ></div>
-            </>
+            </Fragment>
           ))}
         {allPokemonData?.allPokemonSpecies.map(
           (pokemon: GetAllPokemonSpeciesQuery['allPokemonSpecies'][0]) => {
